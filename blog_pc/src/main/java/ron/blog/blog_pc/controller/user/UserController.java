f787e77d9a0c5251110c7df45ed5cd2649016b9b
Ron-Zheng <632815143@qq.com>
2017-08-29 10:02:11 +0800

    log4j2

--- blog_pc/src/main/java/ron/blog/blog_pc/controller/user/UserController.java
+++ blog_pc/src/main/java/ron/blog/blog_pc/controller/user/UserController.java
@@ -1,6 +1,11 @@
 package ron.blog.blog_pc.controller.user;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import ron.blog.blog_pc.controller.base.BaseController;
 
@@ -10,7 +15,20 @@ import ron.blog.blog_pc.controller.base.BaseController;
  * @Date 2017年8月22日 下午3:20:12
  * @return
  */
+@RequestMapping("/user")
 @Controller
 public class UserController extends BaseController {
+	private Logger logger = LogManager.getLogger(this.getClass());
 	
+	/**
+	 * @Comment 用户注册
+	 * @Author Ron
+	 * @Date 2017年8月28日 下午6:01:02
+	 * @return
+	 */
+	@RequestMapping(value="/register")
+	public String register(HttpServletRequest request){
+		logger.info("hello");
+		return "user/register";
+	}
 }
